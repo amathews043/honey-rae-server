9da7ce9eb63e307a333ea5290963e39c4bab9a4c
Annaliese Mathews <amesibov043@gmail.com>
2023-05-02 15:41:30 -0500

    finished chap 10. created the ability for users to create new tickets

--- repairsapi/views/ticket_view.py
+++ repairsapi/views/ticket_view.py
@@ -7,6 +7,22 @@ from repairsapi.models import ServiceTicket, Employee, Customer
 class TicketView(ViewSet): 
     """Honey Rae API Ticket View"""
 
+    def create(self, request):
+        """Handle POST requests for service tickets
+
+        Returns:
+            Response: JSON serialized representation of newly created service ticket
+        """
+        new_ticket = ServiceTicket()
+        new_ticket.customer = Customer.objects.get(user=request.auth.user)
+        new_ticket.description = request.data['description']
+        new_ticket.emergency = request.data['emergency']
+        new_ticket.save()
+
+        serialized = TicketSerializer(new_ticket)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
     def list(self, request): 
         """Handle GET requests to get all tickets"""
 
